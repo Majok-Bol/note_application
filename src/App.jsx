c498b57ed5b59d7cb8c23bf87ae3edabbdbc1c85
Majok-Bol <bolmajok001@gmail.com>
2025-09-10 12:16:05 +0300

    added supabase

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,47 @@
 import { useEffect, useState } from "react";
+import { supabase } from "./supabaseClient.js";
 
 export default function App() {
-  const[note,setNote]=useState("");
+  //track note being set
+  const [note, setNote] = useState("");
+  const [editIndex, setEditIndex] = useState(null);
   //store notes
-  const [storeNotes, setStoreNotes] = useState(()=> {
-    try{
-    //local data on page load
-  return JSON.parse(localStorage.getItem("notes")) || [];
-
-    }catch(error){
-      console.log("Error fetching data: ",error);
-      return [];
-    }
-
-
-  });
+  const [storeNotes, setStoreNotes] = useState([]);
   useEffect(() => {
-    //add notes to local storage
-    localStorage.setItem("notes", JSON.stringify(storeNotes));
-  }, [storeNotes]);
+    async function fetchData(){
+let {data,error}=await supabase.from("notes").select("*");
+console.log(data);
+console.log(error);
+    }
+    fetchData();
+    })
+    
+
   function handleAddNotes() {
-    //get note input
-    //add them on button click
-    setStoreNotes([...storeNotes, note]);
-    //reset input
+    if (note.trim() == ""){
+alert("Note is empty");
+return;
+    } 
+    if (editIndex !== null) {
+      const updatedNotes = [...storeNotes];
+      updatedNotes[editIndex] = note;
+      setStoreNotes(updatedNotes);
+      setEditIndex(null);
+    } else {
+      //add new note
+      const newNote = [...storeNotes, note];
+      setStoreNotes(newNote);
+    }
     setNote("");
   }
-
+  function handleDeleteNotes(index) {
+    const updatedNotes = storeNotes.filter((_, i) => i !== index);
+    setStoreNotes(updatedNotes);
+  }
+  function handleEditNotes(index) {
+    setNote(storeNotes[index]);
+    setEditIndex(index);
+  }
   return (
     <>
       <h1>Note Application</h1>
@@ -35,16 +50,34 @@ export default function App() {
         placeholder="Add notes"
         value={note}
         onChange={(e) => setNote(e.target.value)}
-        onKeyDown={(e)=>e.key==='Enter'&&handleAddNotes()}
+        onKeyDown={(e) => e.key === "Enter" && handleAddNotes()}
       />
-      <button onClick={handleAddNotes}>Add</button>
+      <button onClick={handleAddNotes}>
+        {editIndex !== null ? "Update Note" : "Add Note"}
+      </button>
       <div>
         <h2>Saved Notes</h2>
-        <ul>
-          {storeNotes.map((item, index) => (
-            <li key={index}>{item}</li>
-          ))}
-        </ul>
+        <div>
+          <ul>
+            {storeNotes.map((item, index) => (
+              <li key={index}>
+                {item}
+                <button
+                  className="editBtn"
+                  onClick={() => handleEditNotes(index)}
+                >
+                  Edit
+                </button>
+                <button
+                  className="deleteBtn"
+                  onClick={() => handleDeleteNotes(index)}
+                >
+                  Delete
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </>
   );
